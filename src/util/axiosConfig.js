dcb4e7832d8e1a9195c78ee3041d308a83f1a3d7
Oxana Danilova <oxanadanilova11@gmail.com>
2023-03-11 22:05:58 +0100

    feat: change BE domain

--- src/util/axiosConfig.js
+++ src/util/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://rss-app-project-manager.onrender.com',
+ // baseURL: 'https://rss-app-project-manager.onrender.com',
+ baseURL: 'https://rss-pmapp-be-production.up.railway.app',
 });
 
 export default axiosInstance;
